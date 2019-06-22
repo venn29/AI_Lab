a0491265034c798d6519dbf00631633ac80525d7
venn29 <37021739+venn29@users.noreply.github.com>
2019-06-22 14:01:40 +0800

    loaddata finish

--- Lab2/KNN/KNN.py
+++ Lab2/KNN/KNN.py
@@ -1,17 +1,26 @@
 import csv
 from numpy import *
-def fileread():
-    file = open('trainset.csv','r')
+def fileread(filename):
+    file = open(filename,'r')
     Lines = csv.reader(file)
-    Features = []                    #特征向量初始化，逐行
+    Features = []                   #特征向量初始化，逐行
     Lables = []     #标签
+    i=0
     for line in Lines:
-        Features.append(line[0:5])
-        Lables.append(line[-1])
-    return Features,Lables
+        if(i!=0):           #第一行是0，1，2，3，4，5，6，无用
+            Features.append(line[0:6])
+            Lables.append(line[-1])
+        i = i + 1
+    return array(Features),Lables
 
-Mat,Lable=fileread()
-for item in Lable:
-    print(item)
+def Classfy(InputVector,DataSet,Lable,K):        #用于分类的输入向量,,TrainSet和K
+    LineCount = DataSet.shape[0]     #行数
+    Diff = tile(InputVector,(LineCount,1))-DataSet
+    SQDiff = Diff**2
+    SQsum = SQDiff.sum(axis=1)    #各行求平方和
+    Distance = SQsum**0.5       #欧式距离，每行一个
 
-
+TrainData,TrainLables=fileread('trainset.csv')
+TestData,TrainLables=fileread('testset.csv')
+TestCount = TestData.shape[0]
+fileread('trainset.csv')
