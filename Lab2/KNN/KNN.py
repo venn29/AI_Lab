52605350dee3265bea0c1142f0f67896747d64c8
venn29 <865724378@qq.com>
2019-06-23 00:55:24 +0800

    7 is best

--- Lab2/KNN/KNN.py
+++ Lab2/KNN/KNN.py
@@ -8,14 +8,22 @@ def fileread(filename):
     i=0
     for line in Lines:
         if(i!=0):           #第一行是0，1，2，3，4，5，6，无用
+            for i in range (6):
+                line[i]=ord(line[i])
+                if(line[i]>=ord('0') and line[i]<=ord('9')):
+                    line[i]=line[i]-ord('0')
+                else:
+                    line[i]=line[i]-ord('a')+10
             Features.append(line[0:6])
             Lables.append(line[-1])
         i = i + 1
     return array(Features),Lables
 
 def Classfy(InputVector,DataSet,Lables,K):        #用于分类的输入向量,,TrainSet和K
+    #InputVector=IVector.astype(int)
+    #DataSet=IDataSet.astype(int)
     LineCount = DataSet.shape[0]     #行数
-    Diff = tile(InputVector,(LineCount,1))-DataSet
+    Diff = tile(InputVector,(LineCount,1))-DataSet      #提示可能类型不统一，但是明明是统一的，就很怪
     SQDiff = Diff**2
     SQsum = SQDiff.sum(axis=1)    #各行求平方和
     Distance = SQsum**0.5       #欧式距离，每行一个
@@ -23,11 +31,19 @@ def Classfy(InputVector,DataSet,Lables,K):        #用于分类的输入向量,,
     Count = {}      #存储lable数量的字典,待完善
     for i in range(K):        #选择k个最近邻
         lable = Lables[Indexs[i]]
-        Count[lable]=Count[lable]+1
+        Count[lable]= Count.get(lable,0)+1
+    sortedCount=sorted(Count.items(),key=lambda x:x[1],reverse=True)
+    return sortedCount[0][0]
 
 
 
 TrainData,TrainLables=fileread('trainset.csv')
-TestData,TrainLables=fileread('testset.csv')
+TestData,TestLables=fileread('testset.csv')
 TestCount = TestData.shape[0]
-fileread('trainset.csv')
+ResultLables=[]     #计算出的标签
+error=0
+for i in range(TestCount):
+    ResultLables.append(Classfy(TestData[i],TrainData,TrainLables,7))
+    if(ResultLables[i]!=TestLables[i]):
+        error+=1
+print(error)
