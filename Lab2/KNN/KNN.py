13568d1bf23a4e3477f4a70f8f1e0285617a530b
venn29 <37021739+venn29@users.noreply.github.com>
2019-06-22 15:24:09 +0800

    update Knn.py

--- Lab2/KNN/KNN.py
+++ Lab2/KNN/KNN.py
@@ -13,12 +13,19 @@ def fileread(filename):
         i = i + 1
     return array(Features),Lables
 
-def Classfy(InputVector,DataSet,Lable,K):        #用于分类的输入向量,,TrainSet和K
+def Classfy(InputVector,DataSet,Lables,K):        #用于分类的输入向量,,TrainSet和K
     LineCount = DataSet.shape[0]     #行数
     Diff = tile(InputVector,(LineCount,1))-DataSet
     SQDiff = Diff**2
     SQsum = SQDiff.sum(axis=1)    #各行求平方和
     Distance = SQsum**0.5       #欧式距离，每行一个
+    Indexs=Distance.argsort()       #argsort返回从小到大的索引值
+    Count = {}      #存储lable数量的字典,待完善
+    for i in range(K):        #选择k个最近邻
+        lable = Lables[Indexs[i]]
+        Count[lable]=Count[lable]+1
+
+
 
 TrainData,TrainLables=fileread('trainset.csv')
 TestData,TrainLables=fileread('testset.csv')
