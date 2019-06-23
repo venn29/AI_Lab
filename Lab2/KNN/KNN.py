11fedfabaf2a7dc03d62a3cceaa6a33cd2b31aa3
venn29 <865724378@qq.com>
2019-06-23 19:52:19 +0800

    降维尝试

--- Lab2/KNN/KNN.py
+++ Lab2/KNN/KNN.py
@@ -15,8 +15,18 @@ def fileread(filename):
                     line[i]=line[i]-ord('0')
                 else:
                     line[i]=line[i]-ord('a')+10
-            Kdis = max(abs(line[0]-line[4]),abs(line[1]-line[5]))
-            Features.append(line[0:5])
+            Kingdis = max(abs(line[0]-line[4]),abs(line[1]-line[5]))
+            Cardis = 1
+            if(line[2]!=line[4] and line[3]!=line[5]):
+                Cardis=2
+            #else:  #不考虑三者在同一行并且白国王在中间的情况试试看
+
+            feact.append(Kingdis)
+            feact.append(Cardis)
+            feact.append(line[4])
+            feact.append(line[5])
+            #Features.append(line[0:6])
+            Features.append(feact)
             Lables.append(line[-1])
         i = i + 1
     return array(Features),Lables
